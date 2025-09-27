e533dd472fac69214cf02c4c2d43e442030520a7
Faiz Ur Rehman <faizrehman5765@gmail.com>
2025-09-27 19:05:53 +0500

    Tested the different cards on /Pages/playground.jsx

--- components-playground/src/pages/playground.jsx
+++ components-playground/src/pages/playground.jsx
@@ -3,20 +3,38 @@ import Button from '../components/Button.jsx'
 import Card from '../components/Card.jsx'
 
 function Playground() {
-  
-  return (
-    <>
-      <Card
-        title="This is a title passed as prop"
-        subtitle="A subtitle"
-        actions={<Button />}
-        compact={false}
-      >
-        <p>This is the cards body passed using React's default children prop</p>
-      </Card>
-    </>
 
-  )
+    return (
+        <>
+            <Card
+                title="This is a title passed as prop">
+                <p>Only the title is passed as prop</p>
+            </Card>
+
+
+            <Card
+                title="This is a title passed as prop"
+                subtitle="A subtitle"
+                actions={<Button />}
+                compact={false}
+            >
+                <p>This is the cards body passed using React's default children prop - full card</p>
+            </Card>
+
+
+
+
+            <Card
+                title="This is a title passed as prop"
+                subtitle="A subtitle"
+                actions={<Button />}
+                compact={true}
+            >
+                <p>This is the cards body passed using React's default children prop - full card and compact = true</p>
+            </Card>
+        </>
+
+    )
 }
 
 export default Playground;
