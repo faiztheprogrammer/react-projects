8c86658ac985bf9bd78ce70e8ddc0ae344fd19d6
Faiz Ur Rehman <faizrehman5765@gmail.com>
2025-10-06 01:38:45 +0500

    Practiced more of the Search logic

--- search/src/components/SearchList.jsx
+++ search/src/components/SearchList.jsx
@@ -1,82 +1,140 @@
-import { useState } from 'react'
-import '../App.css'
+// import { useState } from 'react'
+// import '../App.css'
+
+// function SearchList() {
+//   let someData = [
+//     {
+//       id: 1,
+//       title: "Title One",
+//       description: "Some description"
+//     },
+
+//     {
+//       id: 2,
+//       title: "Title Two",
+//       description: "Some description"
+//     },
+
+//     {
+//       id: 3,
+//       title: "Title Three",
+//       description: "Some description"
+//     },
+
+//     {
+//       id: 4,
+//       title: "Title Four",
+//       description: "Some description"
+//     },
+//   ]
+
+//   let [searchText, setSearchText] = useState("")
+
+//   let filteredData = someData.filter((item) => {
+//     const text = searchText.toLowerCase()
+//     const combined = Object.values(item).join(" ").toLowerCase()
+//     return combined.includes(text)
+//   })
+//   return (
+//     <>
+//       <div style={{ textAlign: "center", marginTop: "50px" }}>
+//         <input
+//           value={searchText}
+//           type="search" name="" id="" placeholder='Search items...'
+//           onChange={(e) => setSearchText(e.target.value)}
+//           style={{
+//             padding: "8px 12px",
+//             width: "250px",
+//             borderRadius: "6px",
+//             border: "1px solid #ccc",
+//           }}
+//         />
+
+//         <ul style={{ listStyle: "none", marginTop: "20px", padding: 0 }}>
+//           {filteredData.length > 0 ? (
+//             filteredData.map((item) => (
+//               <li
+//                 key={item.id}
+//                 style={{
+//                   background: "#f2f2f2",
+//                   margin: "8px auto",
+//                   padding: "10px",
+//                   width: "250px",
+//                   borderRadius: "6px",
+//                 }}
+//               >
+
+//                 <strong><p style={{ margin: "5px 0", fontSize: "14px" }}>{item.title}</p></strong>
+//                 <p style={{ margin: "5px 0", fontSize: "12px" }}>
+//                   {item.description}
+//                 </p>
+//               </li>
+//             ))
+//           ) : (
+//             <p>No results found</p>
+//           )}
+//         </ul>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default SearchList;
+
+
+
+
+import { useState } from "react";
 
 function SearchList() {
-  let someData = [
+  let data = [
     {
       id: 1,
-      title: "Title One",
-      description: "Some description"
+      title: "The AI revolution is here!",
+      description: "Extensive use of technology is changing the way we work and operate. Know how!"
     },
 
     {
       id: 2,
-      title: "Title Two",
-      description: "Some description"
+      title: "How looking for external validation weakens a man?",
+      description: "Modern dilemna of our time is to seek external validation at scale."
     },
 
     {
       id: 3,
-      title: "Title Three",
-      description: "Some description"
-    },
-
-    {
-      id: 4,
-      title: "Title Four",
-      description: "Some description"
+      title: "This is how psychological pressure at a younger age shapes our adulthood",
+      description: "One of the great tragedies of our lives is to be pressured so much one is pushed to the edge"
     },
   ]
 
-  let [searchText, setSearchText] = useState("")
+  let [inputText, setInputText] = useState("")
 
-  let filteredData = someData.filter((item) => {
-    const text = searchText.toLowerCase()
-    const combined = Object.values(item).join(" ").toLowerCase()
+  const filteredData = data.filter((item) => {
+    let text = inputText.toLowerCase();
+    let combined = Object.values(item).join(" ").toLowerCase()
     return combined.includes(text)
   })
+
+
   return (
     <>
-      <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <input
-          value={searchText}
-          type="search" name="" id="" placeholder='Search items...'
-          onChange={(e) => setSearchText(e.target.value)}
-          style={{
-            padding: "8px 12px",
-            width: "250px",
-            borderRadius: "6px",
-            border: "1px solid #ccc",
-          }}
+      <div>
+        <input type="text"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
         />
 
-        <ul style={{ listStyle: "none", marginTop: "20px", padding: 0 }}>
-          {filteredData.length > 0 ? (
-            filteredData.map((item) => (
-              <li
-                key={item.id}
-                style={{
-                  background: "#f2f2f2",
-                  margin: "8px auto",
-                  padding: "10px",
-                  width: "250px",
-                  borderRadius: "6px",
-                }}
-              >
-
-                <strong><p style={{ margin: "5px 0", fontSize: "14px" }}>{item.title}</p></strong>
-                <p style={{ margin: "5px 0", fontSize: "12px" }}>
-                  {item.description}
-                </p>
-              </li>
-            ))
-          ) : (
-            <p>No results found</p>
-          )}
-        </ul>
+        <div>
+          {
+            filteredData.map((item) => {
+              <div key={item.id}>
+                <span>{item.title}</span>
+                <span>{item.description}</span>
+              </div>
+            })
+          }
+        </div>
       </div>
     </>
   )
 }
-
-export default SearchList;
