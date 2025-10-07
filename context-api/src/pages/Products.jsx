5fac57aa53e9275920852082135f68c5f0d3398a
Faiz Ur Rehman <faizrehman5765@gmail.com>
2025-10-07 21:43:37 +0500

    Added a static array of dummy data objects in Products.jsx

---
+++ context-api/src/pages/Products.jsx
@@ -0,0 +1,46 @@
+function Products() {
+    let products = [
+        {
+            id: 1,
+            name: "HP Laptop",
+            category: "electronics"
+        },
+
+        {
+            id: 2,
+            name: "Dell Laptop",
+            category: "electronics"
+        },
+
+        {
+            id: 3,
+            name: "Tresseme",
+            category: "Beauty"
+        },
+
+        {
+            id: 4,
+            name: "Sunsilk",
+            category: "Beauty"
+        },
+
+        {
+            id: 5,
+            name: "Air Conditioner",
+            category: "Accessories"
+        },
+
+        {
+            id: 6,
+            name: "General Fan",
+            category: "Accessories"
+        },
+    ]
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Products;
